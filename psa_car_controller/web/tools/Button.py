10d667e19cb31a4ef9516ad1bc0645caa9fc00ab
Jelle <github@jellebuitenhuis.nl>
2023-09-12 20:26:11 +0200

    test

--- psa_car_controller/web/tools/Button.py
+++ psa_car_controller/web/tools/Button.py
@@ -1,6 +1,6 @@
 import dash_bootstrap_components as dbc
 from dash import html
-from dash._utils import create_callback_id
+#from dash._utils import create_callback_id
 from dash.dependencies import Output, Input
 
 from psa_car_controller.web.app import dash_app
